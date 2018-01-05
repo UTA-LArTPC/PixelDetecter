d0a8638b26452ea910d9f644b07c70b72fbb0234
akshattripathi <akshat1tripathi@gmail.com>
2018-01-04 20:30:47 -0600

    Fixed a bug
    The previous script did not plot all the tracks, but just one track/event

--- graph_e.py
+++ graph_e.py
@@ -27,20 +27,20 @@ colors = np.array(intensity)
 norm=plt.Normalize(colors.min(), colors.max())
 
 event_list = ["1", "2", "3", "4", "5"]
-track_list = ["1"]
+track_list = ["1", "2"]
 for eachEvent in range(0, len(event_indices) - 1):
 	event = data[event_indices[eachEvent]:event_indices[eachEvent+1]]
 	event_id = event.split("\n")[0]
 	entries = event.split("\n")
-	x = []
-	y = []
-	z = []
-	dE = []
 
 	if event_id.split()[-1] in event_list:
 		track_indices = list(find_all(event, "Track ID = "))
 		track_indices.append(len(event))
 		for eachTrack in range(0, len(track_indices) - 1):
+			x = []
+			y = []
+			z = []
+			dE = []
 			track = event[track_indices[eachTrack]:track_indices[eachTrack+1]]
 			track_id = track.split("\n")[0].split(",")[0]
 			tracks = track.split("\n")
@@ -56,17 +56,14 @@ for eachEvent in range(0, len(event_indices) - 1):
 							y.append(float(charge.split()[2]))
 							z.append(float(charge.split()[3]))
 							dE.append(float(charge.split()[5]))
-	else:
-		continue
+				fig = plt.figure()
+				ax = fig.add_subplot(111, projection="3d")
+				ax.set_title(event_id + " " + plt_track_id)
+				ax.set_xlabel("X Axis")
+				ax.set_ylabel("Y Axis")
+				ax.set_zlabel("Z Axis")
 
-	fig = plt.figure()
-	ax = fig.add_subplot(111, projection="3d")
-	ax.set_title(event_id + " " + plt_track_id)
-	ax.set_xlabel("X Axis")
-	ax.set_ylabel("Y Axis")
-	ax.set_zlabel("Z Axis")
-
-	for xp, yp, zp, c in zip(x, y, z, colors):
-	    sc = ax.scatter(xp, yp, zp, c=c, cmap=plt.cm.jet , marker="o", norm=norm)
-	plt.colorbar(sc)
-	plt.show()
+				for xp, yp, zp, c in zip(x, y, z, colors):
+				    sc = ax.scatter(xp, yp, zp, c=c, cmap=plt.cm.jet , marker="o", norm=norm)
+				plt.colorbar(sc)
+				plt.show()
